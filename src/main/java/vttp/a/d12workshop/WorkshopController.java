2357bb7d222c3a0081f14a5b483f3a7347c033cc
Gi Eon <95740792+koreanboiboi@users.noreply.github.com>
2022-07-19 14:57:59 +0800

    day 12 workshop - added workshop controller and html

---
+++ src/main/java/vttp/a/d12workshop/WorkshopController.java
@@ -0,0 +1,14 @@
+package vttp.a.d12workshop;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WorkshopController {
+    @RequestMapping (path = {"/workshop"})
+    public String test(){
+        
+        return "workshop";
+    }
+
+}
