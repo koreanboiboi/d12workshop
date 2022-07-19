3d2d59906a962b17fdcc25c34bd14142c0c1cf7d
Gi Eon <95740792+koreanboiboi@users.noreply.github.com>
2022-07-19 09:56:12 +0800

    first commit

---
+++ src/main/java/vttp/a/d12workshop/D12workshopApplication.java
@@ -0,0 +1,13 @@
+package vttp.a.d12workshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D12workshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D12workshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vttp/a/d12workshop/D12workshopApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp.a.d12workshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D12workshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
